8528bb11fdefe88915137216629ce08aa8fd16b0
Aditya Mishra <31aditya0193@gmail.com>
2021-12-28 11:41:40 +0530

    Time: 12 ms (26.51%), Space: 13.8 MB (59.58%) - LeetHub

---
+++ fibonacci-number/fibonacci-number.swift
@@ -0,0 +1,9 @@
+class Solution {
+    func fib(_ n: Int) -> Int {
+        if n == 0 || n == 1 {
+            return n
+        } else {
+            return fib(n - 1) + fib(n - 2)
+        }
+    }
+}
