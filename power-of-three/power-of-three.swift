a0fe3d162d5f5b1a12d6657a5f12f38a1c853f01
Aditya Mishra <31aditya0193@gmail.com>
2021-12-28 12:28:29 +0530

    Time: 56 ms (64.71%), Space: 14.6 MB (25.00%) - LeetHub

---
+++ power-of-three/power-of-three.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func isPowerOfThree(_ n: Int) -> Bool {
+        if n == 0 {
+            return false    //Exceptional case 0
+        }
+        if n == 1 {         //Exceptional case 3^0 = 1
+            return true
+        }
+        if n % 3 == 0 {
+            return isPowerOfThree(n/3)        //Recursive Case check if 3^(n-1) is power of 2.
+        } else {
+            return false                    //Return false if it's not.
+        }
+    }
+}
