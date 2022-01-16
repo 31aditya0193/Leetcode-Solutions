6fc514b71196621d5f6a5d33a4cadc91577b2aba
Aditya Mishra <31aditya0193@gmail.com>
2022-01-16 13:15:32 +0530

    Time: 260 ms (23.83%), Space: 14.6 MB (60.62%) - LeetHub

---
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.swift
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        if root == nil {
+            return root
+        }
+        if root!.val == val {
+            return root
+        }
+        if root!.val > val {
+            return searchBST(root?.left, val)
+        } else {
+            return searchBST(root?.right, val)
+        }
+    }
+}
