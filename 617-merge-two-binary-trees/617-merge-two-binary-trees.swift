3ee17a3a3029d4b1afbcc1794dd1c4371af7960b
Aditya Mishra <31aditya0193@gmail.com>
2022-01-17 19:10:29 +0530

    Time: 160 ms (20.91%), Space: 14.2 MB (85.00%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.swift
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.swift
@@ -15,18 +15,17 @@
  */
 class Solution {
     func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
-        guard let root1 = root1 else {
-            return root2
+        switch (root1?.val, root2?.val) {
+            case (.some(let val1), .some(let val2)):
+                return TreeNode(val1 + val2, 
+                                mergeTrees(root1?.left, root2?.left),
+                                mergeTrees(root1?.right, root2?.right))
+            case (.some(let _), .none):
+                return root1
+            case (.none, .some(let _)):
+                return root2
+            case (.none, .none):
+                return nil
         }
-        
-        guard let root2 = root2 else {
-            return root1
-        }
-        let root = TreeNode(root1.val + root2.val)
-        let left = mergeTrees(root1.left, root2.left)
-        let right = mergeTrees(root1.right , root2.right)
-        root.left = left
-        root.right = right
-        return root
     }
 }
