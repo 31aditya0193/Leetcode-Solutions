3ebb3b9614c964ac29d6737591d143e158250842
Aditya Mishra <31aditya0193@gmail.com>
2022-01-22 16:23:02 +0530

    Time: 53 ms (48.15%), Space: 14.2 MB (24.69%) - LeetHub

--- 733-flood-fill/733-flood-fill.swift
+++ 733-flood-fill/733-flood-fill.swift
@@ -1,9 +1,11 @@
 class Solution {
     var rows: Int
     var columns: Int
+    var visited: Set<[Int]>
     init() {
         rows = 0
         columns = 0
+        visited = Set<[Int]>()
     }
     func floodFill(_ image: [[Int]], _ sr: Int, _ sc: Int, _ newColor: Int) -> [[Int]] {
         var result = image
@@ -15,15 +17,16 @@ class Solution {
     }
     
     func fillColor(_ image: inout [[Int]], _ sr: Int, _ sc: Int, _ targetColor : Int, _ newColor: Int){
-        if sr < 0 || sr >= rows || sc < 0 || sc >= columns || image[sr][sc] != targetColor || image[sr][sc] == newColor {
+        if sr < 0 || sr >= rows || sc < 0 || sc >= columns || image[sr][sc] != targetColor || image[sr][sc] == newColor || visited.contains([sr,sc]) {
             return
         }
         if image[sr][sc] == targetColor {
             image[sr][sc] = newColor
-        }
-        fillColor(&image, sr + 1, sc, targetColor, newColor)
-        fillColor(&image, sr, sc + 1, targetColor, newColor)
-        fillColor(&image, sr - 1, sc, targetColor, newColor)
-        fillColor(&image, sr, sc - 1, targetColor, newColor)
+            visited.insert([sr,sc])
+            fillColor(&image, sr + 1, sc, targetColor, newColor)
+            fillColor(&image, sr, sc + 1, targetColor, newColor)
+            fillColor(&image, sr - 1, sc, targetColor, newColor)
+            fillColor(&image, sr, sc - 1, targetColor, newColor)
+        } else { return }
     }
 }
