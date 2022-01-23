d34f8d0d84132591be3488b8a0814c0213f481bd
Aditya Mishra <31aditya0193@gmail.com>
2022-01-23 11:08:27 +0530

    Time: 52 ms, Memory: 14.1 MB - LeetHub

--- min-cost-climbing-stairs/min-cost-climbing-stairs.swift
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.swift
@@ -1,15 +1,14 @@
 class Solution {
     func minCostClimbingStairs(_ cost: [Int]) -> Int {
-        var table: [Int] = []
         let cnt = cost.count
+        var table: [Int] = Array(repeating: 0, count: cnt)
         for idx in 0..<cnt {
             var num = 0
             if idx < 2 {
-                num = cost[idx]
+                table[idx] = cost[idx]
             } else {
-                num = cost[idx] + min(table[idx-1], table[idx-2])
+                table[idx] = cost[idx] + min(table[idx-1], table[idx-2])
             }
-            table.append(num)
         }
         return min(table[cnt-1], table[cnt-2])
     }
