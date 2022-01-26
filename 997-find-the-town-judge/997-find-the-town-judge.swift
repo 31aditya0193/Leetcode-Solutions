af5e80146d9f3c201e2f470e1fe9745f207c8450
Aditya Mishra <31aditya0193@gmail.com>
2022-01-26 21:50:36 +0530

    Time: 712 ms (99.30%), Space: 14.7 MB (44.21%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.swift
+++ 997-find-the-town-judge/997-find-the-town-judge.swift
@@ -1,15 +1,17 @@
 class Solution {
-    func findJudge(_ n: Int, _ trusts: [[Int]]) -> Int {
-        var trusted = Array(repeating: 0, count: n)
-        for trust in trusts {
-            trusted[trust[0]-1] -= 1
-            trusted[trust[1]-1] += 1
+    func findJudge(_ N: Int, _ trust: [[Int]]) -> Int {
+        var arr = [Int](repeating: 0, count: N+1)
+        let target = N-1
+        var result = [Int]()
+        for one in trust{
+           if arr[one[0]] >= 0 {arr[one[0]] = -1}
+           let a = one[1]
+           arr[a] += 1
         }
-        for idx in 0..<trusted.count {
-            if trusted[idx] == n - 1 {
-                return idx + 1
-            }
+        for i in 1 ... N {
+            if arr[i] == target {return i}
         }
         return -1
+        
     }
 }
