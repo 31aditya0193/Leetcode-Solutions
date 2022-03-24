11c186cc2c6ee2f53d1adb29a04f1e006c0c2ad8
Aditya Mishra <31aditya0193@gmail.com>
2022-03-24 20:55:12 +0530

    Time: 184 ms, Memory: 14.6 MB - LeetHub

--- single-number/single-number.swift
+++ single-number/single-number.swift
@@ -1,13 +1,5 @@
 class Solution {
     func singleNumber(_ nums: [Int]) -> Int {
-        var s = Set<Int>()
-        for num in nums {
-            if s.contains(num) {
-                s.remove(num)
-            } else {
-                s.insert(num)
-            }
-        }
-        return s.first!
+        nums.reduce (0, ^)
     }
 }
