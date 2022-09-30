fbb379c47b9df45db89d2886dfbcb84998fe39f1
Aditya Mishra <31aditya0193@gmail.com>
2022-09-30 09:32:56 +0530

    Time: 916 ms (5.08%), Space: 14.9 MB (76.81%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.swift
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func deepestLeavesSum(_ root: TreeNode?) -> Int {
+        guard let head = root else {
+            return 0
+        }
+        var queue = Array<TreeNode>()
+        queue.append(head)
+        var sum = 0
+        while(queue.count > 0) {
+            sum = 0
+            for _ in 0..<queue.count {
+                let node = queue.removeFirst()
+                sum += node.val
+                if let left = node.left {
+                    queue.append(left)
+                }
+                if let right = node.right {
+                    queue.append(right)
+                }
+            }
+        }
+        return sum
+    }
+}
