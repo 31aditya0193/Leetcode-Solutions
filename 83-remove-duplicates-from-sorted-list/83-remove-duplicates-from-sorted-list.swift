31dc8dca60dec5e55f3c621d193211bfac6197d4
Aditya Mishra <31aditya0193@gmail.com>
2022-10-09 12:12:31 +0530

    Time: 61 ms (15.30%), Space: 13.9 MB (82.72%) - LeetHub

---
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.swift
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        var current = head
+        while (current != nil) {
+            if (current?.val == current?.next?.val) {
+                current?.next = current?.next?.next
+            } else {
+                current = current?.next
+            }
+        }
+        return head
+    }
+}
