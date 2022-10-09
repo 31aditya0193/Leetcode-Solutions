7661b31ce8ed45541f725dd6766674228f69b03c
Aditya Mishra <31aditya0193@gmail.com>
2022-10-09 17:59:30 +0530

    Time: 34 ms (92.52%), Space: 14.3 MB (90.65%) - LeetHub

---
+++ 112-path-sum/112-path-sum.swift
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
+        guard let root = root else {
+            return false
+        }
+
+        if root.val == targetSum && root.right == nil && root.left == nil {
+            return true
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
+    }
+}
