5d16d52e6db53b4c9cd89f6101efacb3a7b22476
Aditya Mishra <31aditya0193@gmail.com>
2022-10-12 12:50:45 +0530

    Time: 9 ms (66.67%), Space: 13.7 MB (16.67%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.swift
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.swift
@@ -1,7 +1,8 @@
 class Solution {
     func minOperations(_ n: Int) -> Int {
+        let count = Int(n/2)
         if n % 2 == 0 {
-            return Int(n/2 * n/2)
+            return count * count
         } else {
             let count = n/2
             return count * (count + 1)
